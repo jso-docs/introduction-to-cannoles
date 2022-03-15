c1a0ebc1af58f72987328800b4e4aed32dafa585
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-03-15 22:44:43 +0100

    Copy tutorial from package repo

--- index.jl
+++ index.jl
@@ -1,34 +1,67 @@
-# # Template for jso-docs separate pages
+#=
 
-# \toc
+## Contents
 
-# Modify this file
+=#
+Pages = ["tutorial.md"]
+#=
 
-# ## Testing
+## Fine-tune CaNNOLeS
 
-# Testing
+CaNNOLeS.jl exports the function `cannoles`:
+#=
+   cannoles(nlp :: AbstractNLPModel; kwargs...)
+#=
 
-A = rand(5, 5)
-b = A * ones(5)
-x = A \ b
+Find below a list of the main options of `cannoles`.
 
-# Testing
-
-using LinearAlgebra
-norm(A * x - b), norm(x .- 1)
+### Tolerances on the problem
 
 #=
-## Plotting
+| Parameters           | Type          | Default         | Description                                        |
+| -------------------- | ------------- | --------------- | -------------------------------------------------- |
+| ϵtol                 | AbstractFloat | √eps(eltype(x)) | tolerance.                                         |
+| unbounded_threshold  | AbstractFloat | -1e5            | below this threshold the problem is unbounded.     |
+| max_f                | Integer       | 100000          | evaluation limit, e.g. `sum_counters(nls) > max_f` |
+| max_time             | AbstractFloat | 30.             | maximum number of seconds.                         |
+| max_inner            | Integer       | 10000           | maximum number of iterations.                      |
+#=
 
-Use `joinpath("__site/assets", filename)` as the path to save the image in the right place and use `{{ rfig filename.ext Caption of image }}` to add the image afterwards.
-It is a good idea to use `# hide` so the save command doesn't appear. See the example below.
+### Algorithmic parameters
+
+#=
+| Parameters                  | Type           | Default           | Description                                        |
+| --------------------------- | -------------- | ----------------- | -------------------------------------------------- |
+| x                           | AbstractVector | copy(nls.meta.x0) | initial guess. |
+| λ                           | AbstractVector | eltype(x)[]       | initial guess for the Lagrange mutlipliers. |
+| method                      | Symbol         | :Newton           | method to compute direction, `:Newton`, `:LM`, `:Newton_noFHess`, or `:Newton_vanishing`. |
+| merit                       | Symbol         | :auglag           | merit function: `:norm1`, `:auglag` |
+| linsolve                    | Symbol         | :ma57             | solver use to compute the factorization: `:ma57`, `:ma97`, `:ldlfactorizations` |
+| check_small_residual        | Bool           | true              | |
+| always_accept_extrapolation | Bool           | false             | |
+| δdec                        | Real           | eltype(x)(0.1)    | |
+#=
+
+## Examples
+
 =#
+using CaNNOLeS, ADNLPModels
 
-using Plots
+# Rosenbrock
+nls = ADNLSModel(x -> [x[1] - 1; 10 * (x[2] - x[1]^2)], [-1.2; 1.0], 2)
+stats = cannoles(nls, ϵtol = 1e-5, x = ones(2))
+#=
 
-x = rand(3)
-y = rand(3)
-plot(x, y)
-png(joinpath("__site/assets", "myplot")) # hide
-
-# {{ rfig myplot.png Example of plot and description }}
+=#
+# Constrained
+nls = ADNLSModel(
+  x -> [x[1] - 1; 10 * (x[2] - x[1]^2)],
+  [-1.2; 1.0],
+  2,
+  x -> [x[1] * x[2] - 1],
+  [0.0],
+  [0.0],
+)
+stats = cannoles(nls, max_time = 10., merit = :auglag)
+#=
+=#
